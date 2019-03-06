d6663d42af9aed4fb5333333ec004dc85ddba840
Chris Franklin <cf73021@gmail.com>
2019-03-06 04:17:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -78,11 +78,5 @@ function forkRepo() {
 
 function getToken() {
   const token = '2ef56a951a3de684ed5934d948d1500aa6fec490';
-
-  fetch('https://api.github.com/user/repos', {
-    headers: {
-      Authorization: `token ${token}`
-    }
-  }).then(res => res.json()).then(json => console.log(json));
   return ''
 }
