bfd7d136166d922f14122314ac25449045d0b8b0
Chris Franklin <cf73021@gmail.com>
2019-03-06 04:18:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,30 +30,30 @@ function showIssues(json) {
 
 function createIssue() {
   var link = 'https://api.github.com/repos/Carpe-Omnia/javascript-fetch-lab/issues' ;
-var title = document.querySelector('#title').value ;
-var body = document.querySelector('#body').value ;
-const token = getToken() ;
-const postData = {
-  title: title,
-  body: body
-};
+  var title = document.querySelector('#title').value ;
+  var body = document.querySelector('#body').value ;
+  const token = getToken() ;
+  const postData = {
+    title: title,
+    body: body
+  };
 
- fetch(link, {
-  method: 'post',
-  body: JSON.stringify(postData),
-  headers: {
-    Authorization: `token ${token}`
-  }
-}).then(getIssues()) ;
-};
+   fetch(link, {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(getIssues()) ;
+  };
 
- fetch(link, {
-  method: 'post',
-  body: JSON.stringify(postData),
-  headers: {
-    Authorization: `token ${token}`
-  }
-}).then(getIssues()) ;
+   fetch(link, {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(getIssues()) ;
 }
 
 function showResults(json) {
